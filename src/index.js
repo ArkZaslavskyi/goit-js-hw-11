3d413411a7a8d2e78f7150a5479a8eb58229336e
ArkZaslavskyi <ark.zaslavskyi@gmail.com>
2022-07-05 11:20:37 +0300

    worked version

--- src/index.js
+++ src/index.js
@@ -1,112 +1,155 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-
-
-/*
-
-// Отправить POST-запрос
-
-axios({
-  method: 'post',
-  url: '/user/12345',
-  data: {
-    firstName: 'Fred',
-    lastName: 'Flintstone'
-  }
-});
-
-// Отправить GET-запрос (метод по умолчанию)
-axios('/user/12345');
-
-
-*/
+import axios from 'axios';
 
 const API_KEY = '28406971-da9ac527785fed0c52df2227a';
 const BASE_URL = 'https://pixabay.com/api/';
+axios.defaults.baseURL = BASE_URL;
 
 const refs = {
     form: document.querySelector('#search-form'),
     searchImgTxt: document.querySelector('[name="searchQuery"]'),
-    loadMoreBtn: document.querySelector('button.load-more'),
+    loadMoreBtn: document.querySelector('.load-more-btn'),
     gallery: document.querySelector('.gallery'),
 };
 
 const cardsPerPg = 40;
 let pgCurrent = 1;
+let totalCards = 0;
 let searchTxt = "";
-const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt' , captionDelay: '250'});
+
+const notifyOptions = {
+    timeout: 5000,
+}
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: '250' });
+
+const observerOptions = {
+    // root: document.querySelector('#scrollArea'),
+    rootMargin: '350px',
+    threshold: 1.0
+};
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            console.log('!!! INTERSECTING !!!');
+            
+            if (pgCurrent * cardsPerPg >= totalCards) {
+                Notify.warning("We're sorry, but you've reached the END of search results.", notifyOptions);
+                return;
+            }
+
+            // 1. HTTP-запрос
+            // 2. Добавить разметку
+            onLoadMoreImages();
+
+        };
+    });
+}, observerOptions);
 
 refs.form.addEventListener('submit', onFormSubmit);
-refs.loadMoreBtn.addEventListener('click', onLoadMoreBtnClk);
+refs.loadMoreBtn.addEventListener('click', onLoadMoreImages);
 
-function onFormSubmit(e) {
-    e.preventDefault();
+// axiosImages();
 
-    searchTxt = e.target.elements.searchQuery.value;
+async function onFormSubmit(e) {
+    try {
+        e.preventDefault();
 
-    // очистка начальных значений
-    resetStartedValues();
+        searchTxt = e.target.elements.searchQuery.value;
 
-    // !!! подумать над очисткой формы !!!
+        // очистка начальных значений
+        resetStartedValues();
 
-    fetchImages()
-        .then(drawGallery)
-        .catch(console.log);
+        // !!! подумать над очисткой формы !!!
+
+        const fetchData = await fetchImages();
+        await drawGallery(fetchData);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+function setObserveOn() {
+    // document.querySelector('#scroll-check').classList.add('scroll-check');
+    observer.observe(document.querySelector('.scroll-check'));
+};
+
+function setObserveOff() {
+    observer.unobserve(document.querySelector('.scroll-check'));
+    // document.querySelector('#scroll-check').classList.remove('scroll-check');
 };
 
 function resetStartedValues() {
     refs.loadMoreBtn.classList.add('is-hidden');
-    refs.form.reset();
     refs.gallery.innerHTML = '';
     pgCurrent = 1;
+    setObserveOff();
 }
 
-function onLoadMoreBtnClk() {
-    pgCurrent += 1;
+async function onLoadMoreImages() {
+    try {
+        pgCurrent += 1;
 
-    fetchImages()
-        .then(drawGallery)
-        .catch(console.log);
+        const fetchData = await fetchImages();
+        await drawGallery(fetchData);
+
+        // observer.observe(document.querySelector('.scroll-check'));
+    } catch (err) {
+        console.error(err);
+    };
 }
 
-function fetchImages() {
-    const requestOptions = new URLSearchParams({
-        q: searchTxt,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-        page: pgCurrent,
-        per_page: cardsPerPg,
-    });
+async function fetchImages() {
+    try {
+        console.log('searchTxt: ', searchTxt);
+        console.log('pgCurrent: ', pgCurrent);
 
-    console.log('searchTxt: ', searchTxt);
-    console.log('pgCurrent: ', pgCurrent);
+        const config = {
+            params: {
+                key: API_KEY,
+                q: searchTxt,
+                image_type: 'photo',
+                orientation: 'horizontal',
+                safesearch: true,
+                page: pgCurrent,
+                per_page: cardsPerPg,
+            },
+        };
 
-    return fetch(`${BASE_URL}?key=${API_KEY}&${requestOptions}`)
-        .then(resp => {
-            if (!resp.ok) {
-                throw Error(resp.status)
-            }
-
-            return resp.json();
-        });
+        const resp = await axios.get('', config);
+        console.log('resp: ', resp);
+        return resp.data;
+    } catch (err) {
+        Notify.failure(err);
+    };
 }
 
-function drawGallery({ hits: cards, totalHits: totalCards }) {
+function drawGallery(data) {
+    // { hits: cards, totalHits: totalCards }
+    const cards = data.hits;
+    totalCards = data.totalHits;
+
     if (!totalCards) {
-        Notify.failure('Sorry, there are NO IMAGES matching your search query. Please try again.');
+        Notify.failure('Sorry, there are NO IMAGES matching your search query. Please try again.', notifyOptions);
+        
+        refs.form.reset();
         return;
     };
 
-    if (pgCurrent === 1) Notify.success(`Hooray! We found ${totalCards} images.`);
+    if (pgCurrent === 1) Notify.success(`Hooray! We found ${totalCards} images.`, notifyOptions);
 
     refs.gallery.insertAdjacentHTML('beforeend', createGalleryMarkup(cards));
 
     lightbox.refresh();
 
+    setObserveOn();
+
     // показать кнопку
-    loadMoreBtnToggle(totalCards);
+    // loadMoreBtnToggle(totalCards);
+
+    // observer.observe(document.querySelector('.scroll-check'));
 };
 
 function loadMoreBtnToggle(totalCards) {
@@ -114,7 +157,7 @@ function loadMoreBtnToggle(totalCards) {
 
     if (pgCurrent * cardsPerPg >= totalCards) {
         refs.loadMoreBtn.classList.add('is-hidden');
-        Notify.warning("We're sorry, but you've reached the END of search results.");
+        Notify.warning("We're sorry, but you've reached the END of search results.", notifyOptions);
     }
 };
 
@@ -126,7 +169,7 @@ function createCardMarkup({webformatURL,largeImageURL,tags,likes,views,comments,
     return `
         <div class="photo-card">
             <a class="gallery-link" href="${largeImageURL}">
-                <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+                <img class="gallery-img" src="${webformatURL}" alt="${tags}" loading="lazy" />
             </a>
             <div class="info">
                 <p class="info-item">
@@ -144,25 +187,4 @@ function createCardMarkup({webformatURL,largeImageURL,tags,likes,views,comments,
             </div>
         </div>
         `;
-/*
-    Шаблон разметки карточки одного изображения для галереи.
-
-    <div class="photo-card">
-        <img src="" alt="" loading="lazy" />
-        <div class="info">
-            <p class="info-item">
-                <b>Likes</b>
-            </p>
-            <p class="info-item">
-                <b>Views</b>
-            </p>
-            <p class="info-item">
-                <b>Comments</b>
-            </p>
-            <p class="info-item">
-                <b>Downloads</b>
-            </p>
-        </div>
-    </div>
- */
 }
